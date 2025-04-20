1eff0162a20be03697700a62c442e1d7feecb896
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-04-20 11:51:18 +0800

    Create a function for formatting the currency

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML;
 
@@ -31,7 +32,7 @@ cart.forEach((cartItem) => {
           ${matchingProduct.name}
         </div>
         <div class="product-price">
-          $${matchingProduct.priceCents / 100}
+          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -93,7 +94,5 @@ cart.forEach((cartItem) => {
     </div>
   </div>`; 
 });
-
-console.log(cartSummaryHTML);
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
