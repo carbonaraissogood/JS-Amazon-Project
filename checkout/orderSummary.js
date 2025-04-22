a0b832135a6490b1cac7f3d706a8414530d584da
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-04-22 19:56:41 +0800

    Refactor order summary to use getProduct for fetching product details

--- checkout/orderSummary.js
+++ checkout/orderSummary.js
@@ -1,5 +1,5 @@
 import { cart, removeFromCart, calculateCartQuantity, updateDeliveryOption } from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import formatCurrency from '../scripts/utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions } from '../../data/deliveryOptions.js'; 
@@ -10,13 +10,7 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
     const productId = cartItem.productId
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
